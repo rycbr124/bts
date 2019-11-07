bcb28fa9ae38b1fbe288f18c8cb402bff3595a9c
종평 <whdvud1213@naver.com>
2019-11-07 15:38:49 +0900

    snsLogin update

--- bts/src/main/java/bts/b/p001/Service/B_P001Service.java
+++ bts/src/main/java/bts/b/p001/Service/B_P001Service.java
@@ -1,11 +1,16 @@
 package bts.b.p001.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import bts.b.p001.VO.B_P001VO;
+import bts.b.p001.VO.KakaoVO;
+import bts.b.p001.VO.NaverVO;
 
 public interface B_P001Service {
 	public void addMember(B_P001VO d001VO) throws Exception;
 	public String overlapped(String id) throws Exception;
 	public B_P001VO login(Map  loginMap) throws Exception;
+	public void kakaoInsert(KakaoVO kakaoVO) throws Exception;
+	public void naverInsert(NaverVO naverVO) throws Exception;
 }
