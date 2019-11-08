2676f771fe078e0a3ebe4b2de4ed0d5233894f80
zootopeanut <ijhwngml@naver.com>
2019-11-08 19:33:37 +0900

    191108 community

--- bts/src/main/java/bts/d/p001_4/controller/D_P001_4ControllerImpl.java
+++ bts/src/main/java/bts/d/p001_4/controller/D_P001_4ControllerImpl.java
@@ -3,10 +3,12 @@ package bts.d.p001_4.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller("d_p001_4")
 @RequestMapping(value="/community")
 public class D_P001_4ControllerImpl implements D_P001_4Controller{
 
@@ -14,8 +16,9 @@ public class D_P001_4ControllerImpl implements D_P001_4Controller{
 	@RequestMapping(value="/plan_list" ,method={RequestMethod.POST,RequestMethod.GET})
 	public ModelAndView searchArticle(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		ModelAndView mav = new ModelAndView("/d/p001_4/d001");
+		return mav;
 		
-		return null;
 	}
 
 }
