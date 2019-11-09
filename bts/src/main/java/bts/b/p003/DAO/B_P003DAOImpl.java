f88aa40c41a3f07855d5ce9c2606c6aaa9c587ea
종평 <whdvud1213@naver.com>
2019-11-09 17:29:55 +0900

    191109

--- bts/src/main/java/bts/b/p003/DAO/B_P003DAOImpl.java
+++ bts/src/main/java/bts/b/p003/DAO/B_P003DAOImpl.java
@@ -3,6 +3,9 @@ package bts.b.p003.DAO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import bts.b.p001.VO.B_P001VO;
 
 
 @Repository("b_p003DAO")
@@ -13,6 +16,13 @@ public class B_P003DAOImpl implements B_P003DAO{
 	@Override
 	public String find_id(String email) throws Exception {
 		return sqlSession.selectOne("mapper.member.find_id",email);
+	}
+
+	@Override
+	@Transactional
+	public int update_pw(B_P001VO p001vo) throws Exception {
+		
+		return sqlSession.update("mapper.member.find_pw",p001vo);
 	}
 
 }
