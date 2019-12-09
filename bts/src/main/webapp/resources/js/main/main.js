39c74a1fcfaca94678e83450b25e5662f188f242
zootopeanut <ijhwngml@naver.com>
2019-12-09 22:57:30 +0900

    main 충돌 수정

--- bts/src/main/webapp/resources/js/main/main.js
+++ bts/src/main/webapp/resources/js/main/main.js
@@ -6,59 +6,56 @@ $(document).ready(function(){
    var reqUrl = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?serviceKey='
         + serviceKey
         + '&contentTypeId=32&areaCode=1&sigunguCode=&cat1=B02&cat2=B0201&cat3=B02010100&listYN=Y&overviewYN=Y&MobileOS=ETC&MobileApp=AppTest&arrange=P&numOfRows=6&pageNo=1&_type=json';
+   var resultArray;
+   $.ajax({
+      async : false,
+      url : reqUrl,
+      dataType : "json",
+      success : function(data, textStatus){
+         resultArray = data.response.body.items.item;
+         for(var i in resultArray){
+            //recommend 숙소 top3
+            var card = document.createElement('div');
+            var card_img_top = document.createElement('img');
+            var card_body = document.createElement('div');
+            var card_title = document.createElement('h5');
+            var card_text = document.createElement('p');
+            $('.best_recommend').append(card);
+            $(card).prop('class','card');
+            $(card).prop('id',resultArray[i].contentid);
+            $(card).prop('style','width:400px;height:250px; display:inline-block; margin:20px; background-color:#FFFAE8; cursor:pointer;');
+            
+            
+            $(card).append(card_img_top);
+            $(card_img_top).prop('class','card-img-top');
+            $(card_img_top).prop('src', resultArray[i].firstimage);
+            $(card_img_top).prop('style', 'width:100%;height:auto; max-height:200px;')
+            
+            $(card).append(card_body);
+            $(card_body).prop('class','card-body');
 
-	var resultArray;
-	var contenttypeid;
-	$.ajax({
-		async : false,
-		url : reqUrl,
-		dataType : "json",
-		success : function(data, textStatus){
-			resultArray = data.response.body.items.item;
-			for(var i in resultArray){
-				//recommend 숙소 top3
-				contenttypeid = resultArray[i].contenttypeid;
-				var card = document.createElement('div');
-				var card_img_top = document.createElement('img');
-				var card_body = document.createElement('div');
-				var card_title = document.createElement('h5');
-				var card_text = document.createElement('p');
-				$('.best_recommend').append(card);
-				$(card).prop('class','card');
-				$(card).prop('id',resultArray[i].contentid);
-				$(card).prop('style','width:400px;height:250px; display:inline-block; margin:20px; background-color:#FFFAE8; cursor:pointer;');
-				
-				
-				$(card).append(card_img_top);
-				$(card_img_top).prop('class','card-img-top');
-				$(card_img_top).prop('src', resultArray[i].firstimage);
-				$(card_img_top).prop('style', 'width:100%;height:auto; max-height:200px;')
-				
-				$(card).append(card_body);
-				$(card_body).prop('class','card-body');
-
-				$(card_body).append(card_title);
-				$(card_title).prop('class','card-title');
-				$(card_title).prop('style','font-weight:bold;');
-				$(card_title).text(resultArray[i].title);
-				
-				$(card_body).append(card_text);
-				$(card_text).prop('class','card-text')
-				$(card_text).prop('style','font-size:12px;');
-				$(card_text).text(resultArray[i].addr1);
-			
-			}
-			$('.card').on('click',function(){
-				var contentid = $(this).attr('id');
-				console.log(contenttypeid);
-				location.href = "/bts/recommend/place_detail?contentid="+contentid + "&contenttypeid=" + contenttypeid;
-			});
-		},
-		error : function(data, textStatus){
-			alert('잘못 된 접근 입니다.');
-		}
-	});
-	  
+            
+            $(card_body).append(card_title);
+            $(card_title).prop('class','card-title');
+            $(card_title).prop('style','font-weight:bold;');
+            $(card_title).text(resultArray[i].title);
+            
+            $(card_body).append(card_text);
+            $(card_text).prop('class','card-text')
+            $(card_text).prop('style','font-size:12px;');
+            $(card_text).text(resultArray[i].addr1);
+         
+         }
+         $('.card').on('click',function(){
+            var contentid = $(this).attr('id');
+            location.href = "/bts/recommend/place_detail?contentid="+contentid;
+         });
+      },
+      error : function(data, textStatus){
+         alert('잘못 된 접근 입니다.');
+      }
+   });
+   
    var reqUrl2 = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?serviceKey='
         + serviceKey
         + '&contentTypeId=15&areaCode=1&sigunguCode=&cat1=A02&cat2=A0207&cat3=&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=Q&numOfRows=5&pageNo=1&_type=json';
