7f849e99578d4b082d2e371bd38cd7fe95faca3a
종평 <whdvud1213@naver.com>
2019-12-16 18:31:19 +0900

    동행글 자신에게 신청 불가처리 & 동행글 검색기능 추가

--- bts/src/main/java/bts/e/p001/Service/E_P001Service.java
+++ bts/src/main/java/bts/e/p001/Service/E_P001Service.java
@@ -9,13 +9,16 @@ import bts.e.p001.VO.E_P001VO;
 import bts.e.p001.VO.PagingVO;
 import bts.e.p003.VO.E_P003VO_2;
 
-
 public interface E_P001Service {
-	 public List selectAllEventList(PagingVO vo) throws DataAccessException;
-	 public List<E_P001VO> selectAccompanyList(PagingVO pagingVO) throws DataAccessException;
-	 public Integer listCount();
-	 public void updateViewcnt(int article_no) throws Exception;
-	 public E_P001VO accView(int article_no) throws Exception;
-	 public List<C_P001VO> inclnView(String member_id) throws Exception;
-	 public List<E_P003VO_2> selectTag(int article_no) throws Exception;
+	public List<E_P001VO> selectAccompanyList(PagingVO pagingVO,String category, String searchResult) throws DataAccessException;
+
+	public Integer listCount();
+
+	public void updateViewcnt(int article_no) throws Exception;
+
+	public E_P001VO accView(int article_no) throws Exception;
+
+	public List<C_P001VO> inclnView(String member_id) throws Exception;
+
+	public List<E_P003VO_2> selectTag(int article_no) throws Exception;
 }
